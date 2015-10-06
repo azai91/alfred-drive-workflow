36847407e3dbd84d1314942c6328e964fc297c22
Alex Zai <azai91@gmail.com>
2015-10-05 21:06:10 -0700

    (feat) remove flask

--- server.py
+++ server.py
@@ -1,23 +1,16 @@
-from flask import Flask, request
 from workflow import Workflow, PasswordNotFound, ICON_TRASH, ICON_WARNING, ICON_USER
 import os
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 from oauth2client.client import OAuth2WebServerFlow
 from action import stopServer
-app = Flask(__name__)
+import urlparse
 
 flow = OAuth2WebServerFlow(client_id='978117856621-tvpnqtr02b8u0bgnh75sqb1loq1f5527.apps.googleusercontent.com',
     client_secret='rty2NIATZfWFWSDX-XPs2usX',
     scope='https://www.googleapis.com/auth/drive',
     redirect_uri='http://localhost:1337')
-
-
-@app.route('/')
-def default():
-    code = request.args.get('code')
-    credentials = flow.step2_exchange(code)
-    wf = Workflow()
-    wf.store_data('google_drive_oauth_code', credentials)
-    return 'Thank you for your code'
 
 def write_pid():
     target = open('./pid.py', 'a')
@@ -25,7 +18,25 @@ def write_pid():
     target.write('\n')
     target.close()
 
-if __name__ == '__main__':
-    stopServer()
-    write_pid()
-    app.run(host='localhost', port=1337)
+class HandlerClass(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        try:
+            code = urlparse.urlparse(s.path)[4].split('=')[1]
+            credentials = flow.step2_exchange(code)
+            wf = Workflow()
+            wf.store_data('google_drive_oauth_code', credentials)
+        except:
+            pass
+        s.wfile.write('Thank you for your code')
+
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.0"
+
+server_address = ('127.0.0.1', 1337)
+
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, HandlerClass)
+write_pid()
+
+sa = httpd.socket.getsockname()
+httpd.serve_forever()
---
+++ test/drive.py
@@ -0,0 +1,90 @@
+import json
+from workflow import Workflow, PasswordNotFound, ICON_TRASH, ICON_WARNING, ICON_USER
+from oauth2client.client import OAuth2WebServerFlow
+import httplib2
+import sys
+import subprocess
+
+flow = OAuth2WebServerFlow(client_id='978117856621-tvpnqtr02b8u0bgnh75sqb1loq1f5527.apps.googleusercontent.com',
+    client_secret='rty2NIATZfWFWSDX-XPs2usX',
+    scope='https://www.googleapis.com/auth/drive',
+    redirect_uri='http://localhost:1337')
+
+log = None
+
+def main(wf):
+    user_input = wf.args[0]
+    command = user_input.split()[0]
+    options = user_input[len(command) + 1:]
+
+    if command == '>':
+        if options in 'login':
+            wf.add_item(title='Drive > login',
+                arg=prepend_action_string('login',get_auth_url()),
+                icon=ICON_USER,
+                subtitle=get_auth_url(),
+                valid=True)
+        if options in 'logout' and wf.stored_data('google_drive_oauth_code'):
+            wf.add_item(title='Drive > logout',
+                arg='logout',
+                icon=ICON_USER,
+                subtitle=get_auth_url(),
+                valid=True)
+        wf.send_feedback()
+        return 0
+    elif wf.stored_data('google_drive_oauth_code'):
+        credentials = wf.stored_data('google_drive_oauth_code')
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        (resp_headers, content) = http.request('https://www.googleapis.com/drive/v2/files?q=title+contains+%22' +command +'%22&key=AIzaSyAMhz8CJf7_xLUquUNdpvTF42fIDk7NALs','GET')
+        results = json.loads(content)['items']
+        for result in results:
+            type = ""
+            try:
+                type = result['mimeType'].split('.')[2]
+            except:
+                type = ""
+            if type == 'spreadsheet':
+                wf.add_item(title=result['title'],
+                    arg=result['alternateLink'],
+                    icon='./assets/sheets.png',
+                    valid=True)
+            elif type == 'document':
+                wf.add_item(title=result['title'],
+                    arg=result['alternateLink'],
+                    icon='./assets/docs.png',
+                    valid=True)
+        if len(results) == 0:
+            wf.add_item(title="No Results Found",
+                icon=ICON_WARNING)
+            wf.send_feedback()
+            return 0
+    else:
+        wf.add_item(title='Drive > login',
+            arg=prepend_action_string('login', get_auth_url()),
+            icon=ICON_USER,
+            subtitle=get_auth_url(),
+            valid=True)
+
+    wf.send_feedback()
+    return 0
+
+def get_auth_url():
+    return flow.step1_get_authorize_url()
+
+def add_credentials(code):
+    try:
+        credentials = flow.step2_exchange(code)
+        wf.store_data('google_drive_oauth_code', credentials)
+        return True
+    except:
+        return False
+
+def prepend_action_string(action, string):
+    return action + string
+
+
+if __name__ == '__main__':
+    wf = Workflow()
+    log = wf.logger
+    sys.exit(wf.run(main))
