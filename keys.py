a329d123beba3e82835539e7c05fbcd2e1538f2e
Alex Zai <azai91@gmail.com>
2015-10-14 17:31:32 -0700

    (feat) move all files to src

--- keys.py
+++
@@ -1,4 +0,0 @@
-client_id = '978117856621-tvpnqtr02b8u0bgnh75sqb1loq1f5527.apps.googleusercontent.com'
-client_secret = 'rty2NIATZfWFWSDX-XPs2usX'
-scope = 'https://www.googleapis.com/auth/drive.readonly'
-redirect_uri = 'http://localhost:1337'
