c3c61ec68fd388500dc43ca72bb44755b76970bd
Alex Zai <azai91@gmail.com>
2015-10-31 11:44:41 -0700

    reduce number of request

--- src/drive.py
+++ src/drive.py
@@ -2,10 +2,10 @@ import json
 from workflow import Workflow, PasswordNotFound, ICON_TRASH, ICON_WARNING, ICON_USER
 import httplib2
 from drive_api import Drive
-from search import Search
 
 UPDATE_SETTINGS = {'github_slug' : 'azai91/alfred-drive-workflow'}
 HELP_URL = 'https://github.com/azai91/alfred-drive-workflow/issues'
+CACHE_MAX_AGE = 60*5 # cache set to 5 minutes
 
 wf = Workflow(update_settings=UPDATE_SETTINGS, help_url=HELP_URL)
 
@@ -29,16 +29,44 @@ def main(_):
     show_options(user_input)
     wf.send_feedback()
   elif len(user_input):
-    search = Search(user_input)
-    Drive.refresh()
+    # Drive.refresh()
     try:
-      search.show_items()
-      wf.logger.error('there are things')
+      show_items(user_input)
     except:
       show_login()
       wf.send_feedback()
 
   return 0
+
+def show_items(user_input):
+  links = wf.cached_data('api_results', Drive.get_links, CACHE_MAX_AGE)
+  try:
+    links = wf.filter(query=user_input,items=links,key=lambda x : x['title'])
+  except:
+    links = []
+
+  if len(links):
+    add_items(links)
+  else:
+    wf.add_item(
+      title='No files found',
+      icon=ICON_WARNING,
+    )
+  wf.send_feedback()
+
+def add_items(links):
+  wf.logger.error('there are links')
+  # sorted(links, key=lambda link : link['lastViewedByMeDate'])
+  for index, link in enumerate(links):
+    title = link['title']
+    alternateLink = link['alternateLink']
+    icon = link['icon']
+    wf.add_item(
+      title=title,
+      arg=alternateLink,
+      icon=icon,
+      valid=True
+    )
 
 def show_login():
   wf.add_item(title='d > login',
--- src/drive_api.py
+++ src/drive_api.py
@@ -9,6 +9,8 @@ flow = OAuth2WebServerFlow(client_id=client_id, client_secret=client_secret, sco
 flow.params['access_type'] = 'offline'
 auth_url = 'https://accounts.google.com/o/oauth2/auth?scope=%s&redirect_uri=%s&response_type=code&client_id=%s&access_type=offline&approval_prompt=force' % (scope,redirect_uri,client_id)
 
+files_url='https://www.googleapis.com/drive/v2/files?orderBy=lastViewedByMeDate+desc'
+
 wf = Workflow()
 
 class DriveExeption(Exception):
@@ -66,7 +68,32 @@ class Drive:
     except:
       wf.logger.error('Error Refreshing')
 
-  # @classmethod
-  # def make_request(cls):
+  @classmethod
+  def get_links(cls):
+    wf.logger.error('CALLED')
+    user_credentials = cls.get_credentials()
+    http = httplib2.Http()
+    wf.logger.error('getting crednetails')
+    http = user_credentials.authorize(http)
+    (resp_headers, content) = http.request(files_url,'GET')
+    wf.logger.error('getting crednetails')
+    unfiltered_list = json.loads(content)['items']
+    return filter_by_file_type(unfiltered_list,['spreadsheet','document'])
 
+def filter_by_file_type(list, file_types):
+  filter_list = []
+  for index, link in enumerate(list):
+    type = ''
+    try:
+      type = link['mimeType'].split('.')[2]
+    except:
+      wf.logger.error('title' + link['mimeType'])
+    if type in file_types:
+      # refactor
+      icon = './icons/sheets.png' if type == 'spreadsheet' else './icons/docs.png'
+      link['icon'] = icon
+      link['type'] = type
+      filter_list.append(link)
+
+  return filter_list
 
