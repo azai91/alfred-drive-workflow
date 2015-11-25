dd00a08d2dd40f77a505d682e780ea0afe855301
Alex Zai <azai91@gmail.com>
2015-11-25 15:54:02 -0800

    (feat) populate cache first

--- src/server.py
+++ src/server.py
@@ -2,18 +2,21 @@ import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from drive_api import Drive
 import urlparse
+from time import sleep
+import subprocess
 
 class HandlerClass(BaseHTTPServer.BaseHTTPRequestHandler):
-  def do_GET(s):
-    try:
-      code = urlparse.urlparse(s.path)[4].split('=')[1]
-      user_credentials = Drive.exchange_tokens(code)
-      s.wfile.write('Your code has been saved in Alfred')
-    except:
-      s.wfile.write('Error with setting code')
+    def do_GET(s):
+        try:
+            code = urlparse.urlparse(s.path)[4].split('=')[1]
+            user_credentials = Drive.exchange_tokens(code)
+            subprocess.call(['python','./drive_refresh.py'])
+            s.wfile.write('Your code has been saved in Alfred')
+        except:
+            s.wfile.write('Error with setting code')
 
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
+ServerClass    = BaseHTTPServer.HTTPServer
+Protocol         = "HTTP/1.0"
 
 server_address = ('127.0.0.1', 1337)
 
